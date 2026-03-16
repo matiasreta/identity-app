5e0c7c7f9fbf16a0ef6820dae0778915e85938fe
matiasreta <matiasrm@outlook.com.ar>
2026-03-16 00:29:52 -0300

    Update DayTimeline.tsx

--- components/TimeTrack/DayTimeline.tsx
+++ components/TimeTrack/DayTimeline.tsx
@@ -257,7 +257,7 @@ const styles = StyleSheet.create({
     },
     objectiveBlock: {
         borderWidth: 1.5,
-        borderStyle: 'solid',
+        borderStyle: 'dashed',
     },
     entryBlock: {
         shadowColor: '#000',
